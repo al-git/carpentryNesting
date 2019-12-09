17d13dccfc51f6f3ec2814114e374a08fb7fc68e
Andrew L. Larson <56542931+al-git@users.noreply.github.com>
2019-12-09 13:24:37 -0500

    Add files via upload

---
+++ carpentryNesting.js
@@ -0,0 +1,34 @@
+var stock; /* Template dimensions of uncut lumber to hold best arrangement
+              of 'wholePiece's */
+
+var cost; // $ cost of 'stock' based on template prices.
+
+
+// Object constructor for working pieces to be derived from 'stock'
+function piece(type, quantity, length, height, width) {
+  this.type =  ; // String name for piece type
+  this.quantity = ; // Int for quantity of 'this.type' needed
+  this.length = ; // Lengths for 'this.type'
+  this.height = ; // Heights for 'this.type'
+  this.width = ; // Heights for 'this.type'
+  this.cutArea = this.lengh * this.width; // Area of this.type when cut
+  this.wholeArea = (this.length + kerf) * (this.width + kerf); /* Area of
+  this.type + kerf for configuring 2D placement within stock
+  this.cutVol = this.length * this.height * this.width; /* Volume of this.type
+  when cut */
+  this.wholeVol = (this.length + kerf) * (this.height + kerf) *
+                  (this.width + kerf);  /* Volume of this.cutVol + kerf to
+  configure 3D arrangement within stock */
+  this.cuts = ; /* # of cuts required to acquire 1 piece from stock. Will often
+  be 4 unless top/bottom/end of stock...how to account for this? */
+  this.waste = ; // Volume of waste if stock consists only of this.type
+}
+
+/*
+// Get list of 'cutPiece'
+function enterPieceDimensiions(piece...) {
+  - Get all of piece.wholeArea
+  - Permutate best ideal arrangement of piece.wholeArea where < stock.area
+  - Generate HTML divs of stock overlaid with piece.wholeArea
+}
+*/
