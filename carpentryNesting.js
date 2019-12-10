f849ae826df21f1b6f4054f415a3a1f146443086
Andrew L. Larson <56542931+al-git@users.noreply.github.com>
2019-12-09 21:31:28 -0500

    General syntax updates and corrections

--- carpentryNesting.js
+++ carpentryNesting.js
@@ -1,28 +1,35 @@
 var stock; /* Template dimensions of uncut lumber to hold best arrangement
               of 'wholePiece's */
 
+function stock(length, height, width) {
+  return new Object
+}
+
 var cost; // $ cost of 'stock' based on template prices.
 
 
 // Object constructor for working pieces to be derived from 'stock'
 function piece(type, quantity, length, height, width) {
-  this.type =  ; // String name for piece type
-  this.quantity = ; // Int for quantity of 'this.type' needed
-  this.length = ; // Lengths for 'this.type'
-  this.height = ; // Heights for 'this.type'
-  this.width = ; // Heights for 'this.type'
-  this.cutArea = this.lengh * this.width; // Area of this.type when cut
-  this.wholeArea = (this.length + kerf) * (this.width + kerf); /* Area of
-  this.type + kerf for configuring 2D placement within stock
-  this.cutVol = this.length * this.height * this.width; /* Volume of this.type
-  when cut */
-  this.wholeVol = (this.length + kerf) * (this.height + kerf) *
-                  (this.width + kerf);  /* Volume of this.cutVol + kerf to
-  configure 3D arrangement within stock */
-  this.cuts = ; /* # of cuts required to acquire 1 piece from stock. Will often
-  be 4 unless top/bottom/end of stock...how to account for this? */
-  this.waste = ; // Volume of waste if stock consists only of this.type
+  return new Object ()
+    this.type =  type, // String name for piece type
+    this.quantity = quantity, // Int for quantity of 'this.type' needed
+    this.length = length, // Lengths for 'this.type'
+    this.height = height, // Heights for 'this.type'
+    this.width = width, // Heights for 'this.type'
+    this.cutArea = this.lengh * this.width, // Area of this.type when cut
+    this.wholeArea = (this.length + kerf) * (this.width + kerf), /* Area of
+    this.type + kerf for configuring 2D placement within stock
+    this.cutVol = this.length * this.height * this.width; /* Volume of this.type
+    when cut */
+    this.wholeVol = (this.length + kerf) * (this.height + kerf) *
+                    (this.width + kerf),  /* Volume of this.cutVol + kerf to
+    configure 3D arrangement within stock */
+    this.cuts = , /* # of cuts required to acquire 1 piece from stock. Will often
+    be 4 unless top/bottom/end of stock...how to account for this? */
+    this.waste = , // Volume of waste if stock consists only of this.type
+  }
 }
+
 
 /*
 // Get list of 'cutPiece'
